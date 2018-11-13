918701baee6870da92a890b70050d8b762ebf244
chaoxn <chaoxn@gmail.com>
2018-11-13 15:47:59 +0800

    init

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by it-ab on 2018/11/13.
+//  Copyright © 2018 ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //
+    }
+}
+
